be922c31bfaf06e0b3afcf8dc866045c59b9ab89
Dan Engel <dengel29@gmail.com>
2024-01-21 10:20:31 +0800

    fix cloudflare adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import lit from "@astrojs/lit";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+// @see https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#modes
 export default defineConfig({
   integrations: [lit()],
   output: "hybrid",
-  adapter: cloudflare({routes: {strategy: 'auto'}, mode: 'advanced'}) // @see https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#modes
+  adapter: cloudflare({routes: {strategy: 'auto'}, mode: 'advanced'}) 
 });
