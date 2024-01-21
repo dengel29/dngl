2f09381ee18b56994f5c38ac0361a03a7f5c4833
Dan Engel <dengel29@gmail.com>
2024-01-21 10:08:13 +0800

    changed astro config to fix SSR bug on deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import lit from "@astrojs/lit";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit(), mdx()],
-  output: "server",
+  integrations: [lit()],
+  output: "hybrid",
   adapter: cloudflare({routes: 'auto', mode: 'advanced'}) // @see https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#modes
 });
