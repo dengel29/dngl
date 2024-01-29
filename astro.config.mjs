6ffe99d4c89c3725bb920c7b49345e9ac26d8094
Dan Engel <dengel29@gmail.com>
2024-01-29 10:37:08 +0800

    try netlify deploy instead of cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import lit from "@astrojs/lit";
-
-import cloudflare from "@astrojs/cloudflare";
+import netlify from '@astrojs/netlify';
+// @see https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#modes
 
 // https://astro.build/config
-// @see https://developers.cloudflare.com/pages/framework-guides/deploy-an-astro-site/#modes
 export default defineConfig({
   integrations: [lit()],
   output: "server",
-  adapter: cloudflare({mode: 'directory'}) 
+  adapter: netlify({edgeMiddleware: true})
 });
